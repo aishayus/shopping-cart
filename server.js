878f6f28101d099a9b7e1b2d434b404ce296b18f
aishayus <aishayussuff2005@gmail.com>
2024-08-14 11:22:51 +0100

    second commit

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}....`);
+    console.log(`Server running on port ${PORT}....`)
 });
